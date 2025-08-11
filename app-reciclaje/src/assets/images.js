372a1c154282df71ae901d84947346f1c504e40d
Nico Barberis <nico09barberis@gmail.com>
2025-08-11 15:51:14 -0300

    nueva carpeta images en public, agregue las imgs de los contenedores

---
+++ app-reciclaje/src/assets/images.js
@@ -0,0 +1,15 @@
+// src/assets/imagenes.js
+
+export const residuesImgs = {
+  botella1: '/images/residuos/botella-plastico.png',
+  papel1: '/images/residuos/papel.png',
+  lata1: '/images/residuos/lata.png',
+  manzana1: '/images/residuos/organico.png',
+}
+
+export const containersImgs = {
+  plastico: '/images/contenedores/plastico.png',
+  papel: '/images/contenedores/papel.png',
+  metal: '/images/contenedores/metal.png',
+  organico: '/images/contenedores/organico.png',
+}
